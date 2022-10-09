9cf761998a669493f3c8c6ae8febb7367ba3c6fa
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-09 15:13:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
+  global.ownername = 'Humanz' // Ganti Jadi Namamu
+  global.owner = ['6288804947282'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6288804947282' // Owner Utama Istilahnya
+  global.botname = 'GojoBotzMD' // Ganti Jadi Nama Botmu
+  global.packname = 'GojoBotzMD' // Sticker Weem
+  global.author = 'Humanz' // Sticker Weem
   global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
   global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
   global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.github = 'https://github.com/OrochimaruBotz' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/humanzboti' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -51,23 +51,23 @@
 
   // Edit Sesuai Kemauan Mu
   global.rpg = {
-  limitEnd: 'Limit Anda Habis\n\n📝 Note : Limit Akan Di Reset Setiap Jam 21:00',
+  limitEnd: 'Limit Anda Habis\n\n📝 Note : Limit Akan Di Reset Setiap Jam 23:00',
   }
 
   // Edit Sesuai Kemauanmu
   global.mess = {
-  admin: '_Lu Admin? 🤨_',
-  botAdmin: '_Bot Bukan Admin 😔_',
-  owner: '_Lu Bukan Owner Gw 😠_',
-  group: '_Khusus Group 😏_',
-  private: '_Khusus Private Chat_',
-  bot: '_Khusus Pengguna Nomer Bot_',
-  wait: '_Sabar Sedang Di Proses ..._',
-  done: '_Done Ngab 🤗_',
+  admin: '_*User Bukanz Seorang Admin*_',
+  botAdmin: '_*GojoBotzMD Bukan Seorang Admin*_',
+  owner: '_*User Bukan Pemilik Bot*_',
+  group: '_*Fitur Khusus Group*_',
+  private: '_*Fitur Khusus Private Chat*_',
+  bot: '_*Khusus Pengguna Nomer Bot*_',
+  wait: '_*Orang Sabar Di Sayang Tuhan*..._',
+  done: '_*Permintaan Sudh Selesai*_',
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
-  global.thumb = fs.readFileSync('./media/hinata.jpg')
+  global.thumb = fs.readFileSync('https://telegra.ph/file/d2475a380a985bdb4c603.jpg')
     
   // Ini Gausah Diubah
   let file = require.resolve(__filename)
